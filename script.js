0990df88675f6f22c03f3d1a03615bad0bcfd32b
Taleb Shaikh <talebshaikh34343434@gmail.com>
2024-12-10 13:24:31 +0530

    minor_adjustments

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ dashboardBtn.addEventListener("click", () => {
   mobileNav.style.display = "flex";
   gsap.to(".close", {
     opacity: 1,
+    ease: "power2.out",
   });
 
   gsap.to(".links ul li a", {
     y: 0,
     opacity: 1,
     duration: 0.5,
-    stagger: 0.1,
+    stagger: 0.05,
+    ease: "power2.out",
   });
 });
 
@@ -27,16 +29,19 @@ closeBtn.addEventListener("click", () => {
   gsap.to(".close", {
     opacity: 0,
     duration: 0.5,
+    ease: "power2.out",
   });
   gsap.to(".links ul li a", {
     y: -500,
     opacity: 0,
     duration: 0.5,
-    stagger: 0.1,
+    stagger: 0.05,
+    ease: "power2.out",
   });
   gsap.to(mobileNav, {
     display: "none",
     duration: 0.5,
+    ease: "power2.out",
   });
 });
 
@@ -54,66 +59,75 @@ gsap.to(".left", {
   opacity: 1,
   x: 0,
   duration: 0.5,
+  ease: "power2.out",
 });
 
 gsap.to(".middle ul li a", {
   y: 0,
   opacity: 1,
-  duration: 1,
+  duration: 0.5,
   ease: "power2.out",
-  stagger: 0.1,
+  stagger: 0.05,
 });
 
 gsap.to(".search-bar", {
   opacity: 1,
   x: 0,
-  duration: 0.7,
+  duration: 0.5,
+  ease: "power2.out",
 });
 
 gsap.to(".dashboard-btn", {
   opacity: 1,
   x: 0,
-  duration: 0.8,
+  duration: 0.5,
+  ease: "power2.out",
 });
 
 gsap.to(".hleft h1", {
   opacity: 1,
   y: 0,
-  duration: 0.4,
+  duration: 0.5,
+  ease: "power2.out",
 });
 
 let tl = gsap.timeline();
-
 tl.to(h1Span, {
   opacity: 1,
   y: 0,
-  duration: 0.2,
-  stagger: 0.5,
-});
+  duration: 1,
+  stagger: 0.25,
+  ease: "power2.out",
+},"a");
 
 tl.to(".hleft>p", {
   opacity: 1,
   x: 0,
-  duration: 0.4,
-});
+  duration: 1,
+  ease: "power2.out",
+},"a");
 
 tl.to(".shop", {
   opacity: 1,
-  duration: 0.1,
-});
+  duration: 1,
+  ease: "power2.out",
+},"a");
 
 tl.to(".hright img", {
   opacity: 1,
   x: 0,
-  duration: 0.2,
-});
+  duration: 1,
+  ease: "power2.out",
+},"a");
 
 tl.to(
   ".bg1",
   {
     opacity: 1,
     x: 0,
-    duration: 0.1,
+    delay: 0.5,
+    duration: 0.5,
+    ease: "power2.out",
   },
   "a"
 );
@@ -123,7 +137,9 @@ tl.to(
   {
     opacity: 1,
     x: 0,
-    duration: 0.1,
+    delay: 0.5,
+    duration: 0.5,
+    ease: "power2.out",
   },
   "a"
 );
